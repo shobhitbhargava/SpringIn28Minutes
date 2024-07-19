1d9e0f212e637389b757b90a41f55d1a88207213
Shobhit Bhargava <shobhitbhargava@yahoo.com>
2024-07-19 12:32:33 +0530

    new commit

--- restful-web-services/src/main/java/com/spring/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/spring/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.spring.rest.webservices.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Rest API
@@ -13,9 +14,16 @@ public class HelloWorldController {
 		return "Hello World";
 	}
 	
+	// This will return the JSON of an hello world bean
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	// Path parameters /hello-world/path-variable/{name}
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 	
 }
