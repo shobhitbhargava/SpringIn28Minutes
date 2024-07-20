cf28f15afa49021644633ffa63111ec2158b3e70
Shobhit Bhargava <shobhitbhargava@yahoo.com>
2024-07-20 14:45:54 +0530

    new commit

--- restful-web-services/src/main/java/com/spring/rest/webservices/restful_web_services/exception/CustomizedResponseEntityExceptionHandler.java
+++ restful-web-services/src/main/java/com/spring/rest/webservices/restful_web_services/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,12 +2,17 @@ package com.spring.rest.webservices.restful_web_services.exception;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.spring.rest.webservices.restful_web_services.user.UserNotFoundException;
 
 @ControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
@@ -19,4 +24,17 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity<Object>(details, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+		ErrorDetails details = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<Object>(details, HttpStatus.NOT_FOUND);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		ErrorDetails details = new ErrorDetails(LocalDateTime.now(), ex.getFieldError().getDefaultMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<Object>(details, HttpStatus.BAD_REQUEST);
+	}
 }
