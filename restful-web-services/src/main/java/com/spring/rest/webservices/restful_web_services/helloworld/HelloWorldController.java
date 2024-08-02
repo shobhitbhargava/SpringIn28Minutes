6703a103e7dbf76b97c9007e91369d27bb91f9da
Shobhit Bhargava <shobhitbhargava@yahoo.com>
2024-08-02 12:34:48 +0530

    new commit, internationalization and content negotiation etc.

--- restful-web-services/src/main/java/com/spring/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/spring/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,17 +1,43 @@
 package com.spring.rest.webservices.restful_web_services.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 //Rest API
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		super();
+		this.messageSource = messageSource;
+	}
 
+	// /hello-world will be shown in the address
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world-old")
+	public String helloWorldOld() {
+		return "Hello World";
+	}
+	
 	// /hello-world will be shown in the address
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	// /hello-world will be shown in the address
+	@GetMapping(path = "/hello-world-internationalization")
+	public String helloWorldInternationalization() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("greetings.questions.message", null, "Default Message", locale);
 	}
 	
 	// This will return the JSON of an hello world bean
--- restful-web-services/src/main/java/com/spring/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/spring/rest/webservices/restful_web_services/user/UserResource.java
@@ -59,7 +59,4 @@ public class UserResource {
 		service.deleteByID(id);
 	}
 	
-	
-	
-	
 }
